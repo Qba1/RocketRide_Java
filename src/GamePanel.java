10d1f50c4c4e60133d43436228fe15978c79b68c
Qba1 <kuba76531@gmail.com>
2024-12-29 22:23:33 +0100

    dodano qiuz

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,11 +7,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
     private Image rocketImage;
     private Image asteroidImage;
-    private Image backgroudImage;
+    private Image menuBackground;
     private Map<String, Image> levelBackgrounds = new HashMap<>();
 
     private int rocketX = 0;
@@ -33,6 +34,8 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
     private boolean inGame = false;
     private int currentLevel = 1;
     private int asteroidCount = 0;
+    private int lives = 3;
+    private boolean quizUsed = false;
 
     private final String[] levelNames = {
             "Merkury", "Wenus", "Ziemia", "Mars", "Jowisz", "Saturn", "Uran", "Neptun"
@@ -48,11 +51,30 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
     private final int[] asteroidsPerLevel = {10, 14, 18, 22, 26, 30, 34, 38};
 
+    private final Map<String, String[]> quizQuestions = Map.of(
+            "Kto był pierwszym człowiekiem w kosmosie?",
+            new String[]{"Yuri Gagarin", "Neil Armstrong", "Buzz Aldrin", "John Glenn"},
+
+            "Która planeta jest największa w Układzie Słonecznym?",
+            new String[]{"Jowisz", "Saturn", "Ziemia", "Mars"},
+
+            "Jak nazywa się pierwszy sztuczny satelita Ziemi?",
+            new String[]{"Sputnik 1", "Voyager 1", "Hubble", "Luna 2"}
+    );
+
+    private final Map<String, Integer> correctAnswers = Map.of(
+            "Kto był pierwszym człowiekiem w kosmosie?", 0,
+            "Która planeta jest największa w Układzie Słonecznym?", 0,
+            "Jak nazywa się pierwszy sztuczny satelita Ziemi?", 0
+    );
+
+    private final Random random = new Random();
+
     public GamePanel() {
         try {
             rocketImage = ImageIO.read(getClass().getClassLoader().getResource("resources/rocket.png"));
             asteroidImage = ImageIO.read(getClass().getClassLoader().getResource("resources/pixelmeteor.png"));
-            backgroudImage = ImageIO.read(getClass().getClassLoader().getResource("resources/background.png"));
+            menuBackground = ImageIO.read(getClass().getClassLoader().getResource("resources/background.png"));
 
             for (String levelName : levelNames) {
                 levelBackgrounds.put(levelName, ImageIO.read(getClass().getClassLoader().getResource("resources/" + levelName.toLowerCase() + ".png")));
@@ -95,70 +117,25 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
         }
 
         drawProgressBar(g);
-
         g.setColor(Color.WHITE);
         g.setFont(new Font("Arial", Font.BOLD, 36));
         g.drawString("Poziom: " + levelNames[currentLevel - 1], WIDTH - 300, 50);
+        g.drawString("Życia: " + lives, 50, 50);
     }
 
     private void drawMenu(Graphics g) {
-        // Narysuj tło
-        g.drawImage(backgroudImage, 0, 0, getWidth(), getHeight(), null);
+        g.drawImage(menuBackground, 0, 0, getWidth(), getHeight(), null);
 
-        // Kolor tekstu i czcionka dla tytułu
-        g.setColor(Color.WHITE);
-        g.setFont(new Font("Arial", Font.BOLD, 72));
+        g.setColor(Color.YELLOW);
+        g.setFont(new Font("Arial", Font.BOLD, 60));
+        g.drawString("Rocket Ride", WIDTH / 2 - 200, HEIGHT / 4);
 
-        // Wyśrodkuj tytuł
-        FontMetrics fmTitle = g.getFontMetrics();
-        int titleWidth = fmTitle.stringWidth("Rocket Ride");
-        int titleHeight = fmTitle.getHeight();
-
-        // Narysuj obramowanie wokół tytułu
-        g.setColor(new Color(100, 150, 255, 200)); // Półprzezroczysty niebieski
-        g.fillRoundRect((WIDTH - titleWidth) / 2 - 20, HEIGHT / 2 - 150 - titleHeight + 10,
-                titleWidth + 40, titleHeight + 20, 20, 20);
-
-        // Obramowanie
-        g.setColor(new Color(255, 255, 255, 200)); // Białe, lekko przezroczyste
-        g.drawRoundRect((WIDTH - titleWidth) / 2 - 20, HEIGHT / 2 - 150 - titleHeight + 10,
-                titleWidth + 40, titleHeight + 20, 20, 20);
-
-        // Narysuj tytuł
-        g.setColor(Color.WHITE);
-        g.drawString("Rocket Ride", (WIDTH - titleWidth) / 2, HEIGHT / 2 - 150);
-
-        // Ustawienia czcionki dla poziomów
         g.setFont(new Font("Arial", Font.PLAIN, 36));
+        g.setColor(Color.WHITE);
+        g.drawString("Wybierz poziom (1-8):", WIDTH / 2 - 150, HEIGHT / 2);
 
-        // Ulepszone wyświetlanie poziomów
-        int startY = HEIGHT / 2;
-        for (int i = 1; i <= 8; i++) {
-            String levelText = String.format("Level %d: %s", i, levelNames[i - 1]);
-
-            // Oblicz szerokość tekstu, aby wyśrodkować
-            FontMetrics fm = g.getFontMetrics();
-            int textWidth = fm.stringWidth(levelText);
-            int textHeight = fm.getHeight();
-
-            // Dodaj obramowanie dla poziomów
-            g.setColor(new Color(100, 150, 255, 220)); // Półprzezroczysty niebieski
-            g.fillRoundRect((WIDTH - textWidth) / 2 - 10, startY + (i - 1) * 50 - textHeight + 10,
-                    textWidth + 20, textHeight + 10, 15, 15);
-
-            // Biała obwódka
-            g.setColor(new Color(255, 255, 255, 200)); // Białe, lekko przezroczyste
-            g.drawRoundRect((WIDTH - textWidth) / 2 - 10, startY + (i - 1) * 50 - textHeight + 10,
-                    textWidth + 20, textHeight + 10, 15, 15);
-
-            // Kolor tekstu poziomów
-//            if (i <= unlockedLevels) {
-//                g.setColor(new Color(50, 50, 150)); // Ciemnoniebieski kolor tekstu
-//            } else {
-//                g.setColor(new Color(100, 100, 100)); // Szary kolor dla zablokowanych poziomów
-//            }
-
-            g.drawString(levelText, (WIDTH - textWidth) / 2, startY + (i - 1) * 50);
+        for (int i = 0; i < levelNames.length; i++) {
+            g.drawString((i + 1) + ": " + levelNames[i], WIDTH / 2 - 100, HEIGHT / 2 + (i + 1) * 40);
         }
     }
 
@@ -191,9 +168,13 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
             asteroid.x -= asteroidSpeed;
 
             if (checkCollision(asteroid)) {
-                timer.stop();
-                JOptionPane.showMessageDialog(this, "Game Over!");
-                System.exit(0);
+                lives--;
+                toRemove.add(asteroid);
+                if (lives == 0 && !quizUsed) {
+                    showQuiz();
+                } else if (lives == 0) {
+                    gameOver();
+                }
             }
 
             if (asteroid.x + ASTEROID_WIDTH < 0) {
@@ -228,6 +209,36 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
         return rocketBounds.intersects(asteroidBounds);
     }
 
+    private void showQuiz() {
+        quizUsed = true;
+        String question = new ArrayList<>(quizQuestions.keySet()).get(random.nextInt(quizQuestions.size()));
+        String[] answers = quizQuestions.get(question);
+        int correctAnswer = correctAnswers.get(question);
+
+        String userAnswer = (String) JOptionPane.showInputDialog(this, question, "Quiz", JOptionPane.QUESTION_MESSAGE, null, answers, answers[0]);
+        if (userAnswer != null && userAnswer.equals(answers[correctAnswer])) {
+            lives = 1;
+            JOptionPane.showMessageDialog(this, "Poprawna odpowiedź! Zyskujesz dodatkowe życie.");
+        } else {
+            gameOver();
+        }
+    }
+
+    private void gameOver() {
+        JOptionPane.showMessageDialog(this, "Gra zakończona! Wracasz do menu.");
+        resetGame();
+    }
+
+    private void resetGame() {
+        inGame = false;
+        lives = 3;
+        currentLevel = 1;
+        asteroidCount = 0;
+        asteroidSequence.clear();
+        asteroids.clear();
+        repaint();
+    }
+
     private void showLevelCompleteDialog() {
         int option = JOptionPane.showOptionDialog(
                 this,
@@ -252,11 +263,11 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
         currentLevel++;
         if (currentLevel > levelNames.length) {
             JOptionPane.showMessageDialog(this, "Gratulacje! Ukończyłeś całą grę!");
-            System.exit(0);
+            resetGame();
         }
         asteroidSequence = new ArrayList<>(levelSequences.get(currentLevel - 1));
         sequenceIndex = 0;
-        asteroidSpeed += 5; // Przyspieszenie asteroid
+        asteroidSpeed += 5;
     }
 
     @Override
@@ -264,6 +275,8 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
         if (!inGame) {
             if (e.getKeyCode() >= KeyEvent.VK_1 && e.getKeyCode() <= KeyEvent.VK_8) {
                 currentLevel = e.getKeyCode() - KeyEvent.VK_0;
+
+
                 asteroidSequence = new ArrayList<>(levelSequences.get(currentLevel - 1));
                 inGame = true;
             }
